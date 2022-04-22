9b0479944f7fb0a87a89de5965732d4247c1caaf
Yanjun Chen <51148553+markyanjunch@users.noreply.github.com>
2022-04-21 21:59:53 -0400

    Add data

---
+++ src/utils.js
@@ -0,0 +1,34 @@
+export const Songs = () => {
+    return [
+        {
+            title: "Lost Stars",
+            artist: "Adam Levine",
+            cover: "./data/music/lost_stars.mp3",
+            audio: "./data/music/lost_stars.jpg"
+        },
+        {
+            title: "Bitter Gourd (苦瓜)",
+            artist: "Eason Chan (陈奕迅)",
+            cover: "./data/music/bitter_gourd.mp3",
+            audio: "./data/music/bitter_gourd.jpg"
+        },
+        {
+            title: "Love Confession (告白气球)",
+            artist: "Jay Chou (周杰伦)",
+            cover: "./data/music/love_confession.mp3",
+            audio: "./data/music/love_confession.jpg"
+        },
+        {
+            title: "Feel Special",
+            artist: "Twice",
+            cover: "./data/music/feel_special.mp3",
+            audio: "./data/music/feel_special.jpg"
+        },
+        {
+            title: "Yellow",
+            artist: "Coldplay",
+            cover: "./data/music/yellow.mp3",
+            audio: "./data/music/yellow.jpg"
+        }
+    ];
+}
